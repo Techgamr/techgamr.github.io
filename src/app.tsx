08550a503f139438f0086f6f71ec1d8217a154ff
Techgamr <60068447+Techgamr@users.noreply.github.com>
2021-03-14 18:29:38 +0000

    Add a few things

--- src/app.tsx
+++ src/app.tsx
@@ -5,7 +5,7 @@ export default class App extends React.Component<{}, {}>{
     render() {
         return (
             <div className={styles.app}>
-
+                <h1 className={styles.appText}>Hi 👋</h1>
             </div>
         );
     }
