2b57008916b93cecb9e41ad7133d2a88c4070d39
Techgamr <60068447+Techgamr@users.noreply.github.com>
2021-04-09 15:32:45 +0100

    Optimise

--- src/app.tsx
+++ src/app.tsx
@@ -1,22 +1,52 @@
 import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
-import Homepage from "./pages/Homepage";
-import About from "./pages/About";
-import Header from "./header";
+const Header = React.lazy(() => import("./header"));
+const Homepage = React.lazy(() => import("./pages/Homepage"));
+const About = React.lazy(() => import("./pages/About"));
 
 export const headerText = "Hi 👋";
+
+const bodySuspenseFallback = (
+  <div
+    style={{
+      width: "100%",
+      height: "100%",
+      backgroundColor: "var(--bg-color)",
+      display: "flex",
+      flexDirection: "row",
+      alignItems: "center",
+      justifyContent: "center",
+    }}
+  ></div>
+);
 
 export default class App extends React.Component<{}, {}> {
   render() {
     return (
       <BrowserRouter>
-        <Header />
+        <React.Suspense
+          fallback={
+            <div
+              style={{
+                width: "100%",
+                height: "36px",
+                backgroundColor: "var(--bg-color)",
+              }}
+            ></div>
+          }
+        >
+          <Header />
+        </React.Suspense>
         <Switch>
           <Route exact path={"/"}>
-            <Homepage />
+            <React.Suspense fallback={bodySuspenseFallback}>
+              <Homepage />
+            </React.Suspense>
           </Route>
           <Route path={"/about"}>
-            <About />
+            <React.Suspense fallback={bodySuspenseFallback}>
+              <About />
+            </React.Suspense>
           </Route>
         </Switch>
       </BrowserRouter>
